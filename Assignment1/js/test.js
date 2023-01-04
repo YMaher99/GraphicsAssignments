39586716bf5a4af29285552ab93b76afb7284b0b
YMaher99 <youssefmaher725@gmail.com>
2023-01-04 12:07:13 +0200

    Added sounds and difficulty to assignment 1

--- Assignment1/js/test.js
+++ Assignment1/js/test.js
@@ -3,9 +3,49 @@ import * as THREE from 'https://cdn.skypack.dev/three@0.136';
 import { GUI } from 'https://cdn.skypack.dev/three@0.136/examples/jsm/libs/lil-gui.module.min.js';
 
 const scene = new THREE.Scene();
-var score = 0;
 
 const camera = new THREE.PerspectiveCamera( 120, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const listener = new THREE.AudioListener();
+const audioLoader = new THREE.AudioLoader();
+
+var difficulty = null;
+var fruitSpeed = 0.05;
+
+while(!difficulty){
+	difficulty = prompt("Please enter the difficulty E/N/H", "N").toLowerCase();
+
+	switch(difficulty){
+		case "e":
+			fruitSpeed = 0.02;
+			break;
+		case "n":
+			fruitSpeed = 0.05;
+			break;
+		case "h":
+			fruitSpeed = 0.13;
+			break;
+		default:
+			fruitSpeed = 0.05;
+			break;
+	}
+}
+
+const gotFruitSound = new THREE.Audio(listener);
+audioLoader.load("./got fruit.mp3", function(buffer){
+	gotFruitSound.setBuffer(buffer);
+	gotFruitSound.setLoop(false);
+	gotFruitSound.setVolume(1.0);
+	gotFruitSound.duration = 1.5;
+});
+
+const failureSound = new THREE.Audio(listener);
+audioLoader.load("./failure.mp3", function(buffer){
+	failureSound.setBuffer(buffer);
+	failureSound.setLoop(false);
+	failureSound.setVolume(1.0);
+
+});
 
 const renderer = new THREE.WebGLRenderer({alpha:true});
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -21,7 +61,7 @@ const ambient = new THREE.AmbientLight(0xffffff);
 scene.add(ambient);
 
 const pointLight = new THREE.PointLight(0xffffff,15);
-pointLight.position.set(0, 20, -3);
+pointLight.position.set(0, 23, -6);
 pointLight.castShadow = true;
 scene.add(pointLight);
 
@@ -30,18 +70,23 @@ camera.position.y = 15;
 
 scene.background = new THREE.TextureLoader().load( "./background.jpg" );
 
-var fruitExists = false;
 
+
+
+var fruitExists = false;
+var failures = 0;
 
 
 var gui = new GUI();
 var myObject = {
 	Score: 0,
+	Lives: 3
 };
 
 
 gui.add(myObject,'Score');
-gui.title("SCORE")
+gui.add(myObject,"Lives");
+gui.title("")
 
 
 
@@ -59,7 +104,7 @@ window.addEventListener('keydown', function (e) {
 
 });
 
-
+console.log(fruitSpeed)
 
 function isColliding(obj1, obj2) {
 	if (obj1) {
@@ -79,25 +124,40 @@ function animate() {
 		fruitExists = true;
 		scene.add(droppedCube);
 		droppedCube.position.y=30;
-		droppedCube.position.x = Math.random() * 30 ;
-
+		if(Math.random()>0.5){
+			droppedCube.position.x = Math.random() * 30 ;
+		}
+		else{
+			droppedCube.position.x = -Math.random() * 30;
+		}
 	}
 	if(fruitExists){
-		droppedCube.position.y -=0.05;
+		droppedCube.position.y -= fruitSpeed;
 		if(isColliding(cube,droppedCube)){
-			score +=1
-			console.log(score);
+			gotFruitSound.play()
 			scene.remove(droppedCube);
 			fruitExists = false;
 			gui.controllers.forEach(element => {
-				element.setValue(element.getValue() + 1)
+				if(element.property == "Score"){
+					element.setValue(element.getValue()+1)
+				}
 			});
 
 		}
 		if(droppedCube.position.y<0){
-			console.log("fell");
+			failures+=1;
 			scene.remove(droppedCube);
 			fruitExists = false;
+			failureSound.play()
+			gui.controllers.forEach(element => {
+				if(element.property == "Lives"){
+					element.setValue(element.getValue()-1)
+				}
+			});
+			if(failures>=3){
+				alert("You lost!")
+				location.reload()
+			}
 		}
 	}
 
